1c404af8f50a4f324496d47ec753dea92606aa4c
Jamie English <jamienglish@gmail.com>
2015-03-29 23:26:00 +0100

    Add a test

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  [ring "1.3.1"]
                  [ring/ring-devel "1.1.8"]
                  [ring/ring-json  "0.3.1"]
+                 [ring/ring-mock  "0.2.0"]
                  [cheshire "5.2.0"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [load-test "0.1.0-SNAPSHOT"]])
--- test/load_test_server/core_test.clj
+++ test/load_test_server/core_test.clj
@@ -1,7 +1,16 @@
 (ns load-test-server.core-test
   (:require [clojure.test :refer :all]
-            [load-test-server.core :refer :all]))
+            [load-test-server.core :refer :all]
+            [clojure.data.json :as json]
+            [ring.mock.request :as mock]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest presets-test
+  (testing "GET /presets"
+    (let [{:keys [status headers body]} ((app) (mock/request :get "/presets"))]
+      (is (= 200 status))
+      (is (= "application/json; charset=utf-8" (get headers "Content-Type")))
+      (is (= {:resources {:creditors ["index" "create"]}
+              :url "api-staging.gocardless.com"
+              :duration 5
+              :rate 3}
+             (json/read-str body :key-fn keyword))))))
